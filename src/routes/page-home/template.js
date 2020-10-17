ccbfee570c3648db4070343c7fdcb4f8e2cb1200
Toni Jan Monserrat <tonijanmonserrat@gmail.com>
2020-10-17 12:19:33 +0800

    fix: remove bits

--- src/routes/page-home/template.js
+++ src/routes/page-home/template.js
@@ -1,22 +1,15 @@
 import { html } from 'lit-html';
 import style from './style.component.scss';
-import '../../components/blog/blog-list-loader';
-import '../../components/blog/blog-list-component';
 
 /**
  * @this {import('./index').Page & LitPage}
  */
 export function template () {
-  const { queryObject } = this;
-  const { nextId, prevId } = queryObject;
-
   return html`
     <style>
       ${style}
     </style>
 
-    <blog-list-loader .nextId="${nextId}" .prevId="${prevId}">
-      <blog-list-component></blog-list-component>
-    </blog-list-loader>
+    Hello World
   `;
 }
